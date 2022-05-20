6525b2521681be5cf6a626ed7f0358633b7a4939
Zakaria <zaktakk96@gmail.com>
2022-05-20 19:28:40 +0300

    V5.0 - Added Game Reset oprion when clicked on S

--- game.js
+++ game.js
@@ -18,12 +18,23 @@ function start_game() {
     }
 }
 
+// reset-game function (click on S)
+function reset_game() {
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].classList.remove("youlose")
+    }
+    score = 0
+    board[0].innerHTML = "Score reset!" + "</br> Score:" + score 
+    board[0].style.height = "auto"
+    board[0].style.width = "fit-content"
+}
+
 // losing function - when touching walls
 function you_lost() {
     for (let i = 0; i < walls.length; i++) {
         walls[i].classList.add("youlose")
     }
-    board[0].innerHTML = "YOU LOST!" + "Score:" + score 
+    board[0].innerHTML = "YOU LOST!" + "</br> Score:" + score 
     board[0].style.height = "auto"
     board[0].style.width = "fit-content"
     score -= 10
@@ -41,6 +52,9 @@ function you_won() {
 
 // start game:
 game_start.addEventListener("mouseover", start_game);
+
+// reset game
+game_start.addEventListener("click", reset_game);
 
 // win: 
 game_end.addEventListener("mouseover", you_won);

4ed9c8e56791de23073c0e403660378b28f289f1
Zakaria <zaktakk96@gmail.com>
2022-05-20 09:44:35 +0300

    V2 - Initializing JS Variables

--- game.js
+++ game.js
@@ -1,3 +1,24 @@
-window.onunload = function() {
+ // window.onunload = function {}
+ 
+// initialize event variables
 
+ var game_start = document.getElementById("start")
+ var game_end = document.getElementById("end")
+ var walls = document.getElementById("boundary1")
+ var board = document.getElementsByClassName("boundary example")
+ 
+// declare score
+var score
+
+// losing function - when touching walls
+function you_lost() {
+   // for(var i = 0; i < walls.length; i++) {
+        walls.classList.add("youlose")
+   // }
 }
+
+// start-game function
+
+
+walls.addEventListener("mouseover", you_lost );
+

124474849dd0b887918db91adf9b1ffc44fae421
Zakaria <zaktakk96@gmail.com>
2022-05-20 21:18:45 +0300

    V7.0 - Organized events in functions

--- game.js
+++ game.js
@@ -20,17 +20,20 @@ function start_game() {
     for (let i = 0; i < walls.length; i++) {
         walls[i].classList.remove("youlose")
     }
+    game_end.addEventListener("mouseover", you_won);
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].addEventListener("mouseover", you_lost);
+    }
 }
 
 // reset-game function (click on S)
 function reset_game() {
-    for (let i = 0; i < walls.length; i++) {
-        walls[i].classList.remove("youlose")
-    }
     score = 0
     board[0].innerHTML = "Score reset!" + "</br> Score:" + score 
     board[0].style.height = "auto"
     board[0].style.width = "fit-content"
+    // Call the game start function
+    start_game()
 }
 
 // losing function - when touching walls
@@ -42,6 +45,14 @@ function you_lost() {
     board[0].innerHTML = "YOU LOST!" + "</br> Score:" + score 
     board[0].style.height = "auto"
     board[0].style.width = "fit-content"
+    // Disable winning
+    game_end.removeEventListener("mouseover", you_won);
+    // disable losing again
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].removeEventListener("mouseover", you_lost);
+    }    
+    // enable the abilty to start again
+    game_start.addEventListener("mouseover", start_game);
 }
 
 // winning function - when touching end
@@ -50,22 +61,42 @@ function you_won() {
     board[0].innerHTML = "YOU WON!" + "</br> Score:" + score
     board[0].style.height = "auto"
     board[0].style.width = "fit-content"
+    // Disable winning again
+    game_end.removeEventListener("mouseover", you_won);
+    // Disable losing
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].removeEventListener("mouseover", you_lost);
+    }    
+    // Enable the abilty to start again
+    game_start.addEventListener("mouseover", start_game);
 }
 
 // Game utilities
 
-// start game:
+// start game - Game Initializer:
 game_start.addEventListener("mouseover", start_game);
 
-// reset game
-game_start.addEventListener("click", reset_game);
+// reset game:
+// game_start.addEventListener("click", reset_game);
 
 // win: 
-game_end.addEventListener("mouseover", you_won);
+// game_end.addEventListener("mouseover", you_won);
 
 // lose:
+/* 
 for (let i = 0; i < walls.length; i++) {
     walls[i].addEventListener("mouseover", you_lost);
 }
+*/
 
+/* sequence of the game:
+When move over S:
+enable win adn lose
 
+lose:
+disable win 
+enable start
+
+when win:
+disable win and lose
+enable start*/

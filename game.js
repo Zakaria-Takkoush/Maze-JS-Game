0349ed6041a6ee64b439fe894ce9c99e8c7d4275
Zakaria <zaktakk96@gmail.com>
2022-05-20 11:20:08 +0300

    V3.0 - Added losing function

--- game.js
+++ game.js
@@ -4,21 +4,39 @@
 
  var game_start = document.getElementById("start")
  var game_end = document.getElementById("end")
- var walls = document.getElementById("boundary1")
+ var walls = document.getElementsByClassName("boundary")
  var board = document.getElementsByClassName("boundary example")
+ var game = document.getElementById("game")
  
 // declare score
 var score
 
-// losing function - when touching walls
-function you_lost() {
-   // for(var i = 0; i < walls.length; i++) {
-        walls.classList.add("youlose")
-   // }
+// start-game function
+function start_game() {
 }
 
-// start-game function
+// losing function - when touching walls
+function you_lost() {
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].classList.add("youlose")
+    }
+    score -= 10
+}
 
+// winning function - when touching end
+function you_won() {
+    score += 5
+}
 
-walls.addEventListener("mouseover", you_lost );
+// Game utilities
+
+// start game:
+
+// win: 
+
+// lose:
+for (let i = 0; i < walls.length; i++) {
+    walls[i].addEventListener("mouseover", you_lost);
+}
+
 

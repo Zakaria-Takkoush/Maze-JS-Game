4ee153fb44f879cfd578fc672dff7c6493f0a44f
Zakaria <zaktakk96@gmail.com>
2022-05-20 12:23:37 +0300

    V4.0 - Added win/lose/start functions and events

--- game.js
+++ game.js
@@ -9,10 +9,13 @@
  var game = document.getElementById("game")
  
 // declare score
-var score
+var score = 0
 
 // start-game function
 function start_game() {
+    for (let i = 0; i < walls.length; i++) {
+        walls[i].classList.remove("youlose")
+    }
 }
 
 // losing function - when touching walls
@@ -20,23 +23,34 @@ function you_lost() {
     for (let i = 0; i < walls.length; i++) {
         walls[i].classList.add("youlose")
     }
+    board[0].innerHTML = "YOU LOST!" + "Score:" + score 
+    board[0].style.height = "auto"
+    board[0].style.width = "fit-content"
     score -= 10
 }
 
 // winning function - when touching end
 function you_won() {
+    board[0].innerHTML = "YOU WON!" + "</br> Score:" + score
+    board[0].style.height = "auto"
+    board[0].style.width = "fit-content"
     score += 5
 }
 
 // Game utilities
 
 // start game:
+game_start.addEventListener("mouseover", start_game);
 
 // win: 
+game_end.addEventListener("mouseover", you_won);
 
 // lose:
 for (let i = 0; i < walls.length; i++) {
     walls[i].addEventListener("mouseover", you_lost);
 }
+
+
+
 
 
